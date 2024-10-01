8f2b934ef331b2f0b40e598e434d806c7a3e85fd
ivyvanzant <ivyvanzant@gmail.com>
2024-10-01 16:20:40 -0700

    finished with step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,9 +6,54 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.send(users);
+  const users = {
+    users_list: [
+      { 
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
+  app.get("/users", (req, res) => {
+    res.send(users);
+  });
+
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
+
 
 app.listen(port, () => {
   console.log(
@@ -17,49 +62,5 @@ app.listen(port, () => {
 
 });
 
-const users = {
-  users_list: [
-    { 
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
 
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
-});
